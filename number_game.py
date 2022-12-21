3192907a77287c0b86cb1a18b2a40be1ccbb911a
BooAngeldust <alex241001@outlook.com>
2022-12-21 18:52:27 +0000

    initial commit

---
+++ number_game.py
@@ -0,0 +1,31 @@
+import random
+
+def numberGame():
+    number : int = random.randint(1,101)
+
+    print("Guess the number in 1-100 range")
+    print(f"DEBUG: {number}") 
+    while True:
+        numberGuess : int = int(input(">>> "))
+        if numberGuess > number:
+            print("Try lower")
+        elif numberGuess < number:
+            print("Try higher")
+        else:
+            print("You won!")
+            break
+
+name : str = input("Enter your name: ")
+
+while True:
+    choice : str = input(f"Hello {name} would you like to play guess the number game? (y/n): ")
+
+    if choice.lower() == "y":
+        numberGame()
+        break
+    elif choice.lower() == "n":
+        break
+    else:
+        print("Invalid input")
+
+print("Bye")
